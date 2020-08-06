3ee63e911d6a0fba206900885f5958fff7ee358f
rcai7774 <rcai7774@gmail.com>
2020-08-06 05:04:08 +0000

    change as always

--- words/analyzer.js
+++ words/analyzer.js
@@ -1,7 +1,7 @@
 function analyze(text){
 	debugger;
-	text = text.replace(/[\,\.\(\)\&\{\}\[\]\`\~\:\;\"\<\>\?\/]/g,'');
-	text = text.replace(/[\-\_\/]/g, ' ');
+	text = text.toLowerCase().replace(/[\,\=\.\(\)\&\{\}\[\]\`\~\:\;\"\<\>\?\/]/g,'');
+	text = text.toLowerCase().replace(/[\-\_\/]/g, ' ');
 	let words = text.split(" ");
 	
 	const wordMap = new Map();
@@ -12,26 +12,42 @@ function analyze(text){
 			wordMap.set(words[i] , 1);
 		}
 	}
-	display(wordMap);
+	let sum = frequency(wordMap);
+	let entries = wordMap.entries();
+	let sortedEntries = Array.from(entries).sort(compareFunction);
+	display(sum, sortedEntries);
 }
-function display(wordMap){
+function display(sum, sortedEntries){
+	
 	let tblHtml = `
 						 <table style = "background-color:#ccFFFF; text-align:center;">
 						 <tr>
     <th><u>Word</u></th>
     <th><u>#Of Occurences</u></th> 
+		<th><u>Frequency</u></th>
   </tr>`
-	for(const k of wordMap.entries()){
-		tblHtml += createRow(k)
+	for(const k of sortedEntries){
+		tblHtml += createRow(k , sum)
 				 }
              tblHtml += `</table>`
 						 out.innerHTML = tblHtml;
 }
-const createRow = function(k){
+const createRow = function(k , sum){
            let rowHtml = `<tr>
 					 <td> ${k[0]} </td>
 					 <td> ${k[1]} </td>
+					 <td> ${(100*(k[1] / sum)).toFixed(2)}%</td>
 					 </tr>
 					 `
            return rowHtml;
+}
+function frequency(wordMap){
+	let sum = 0;
+	for(const m of wordMap.values()){
+		sum += m;
+	}
+	return sum;
+}
+const compareFunction = function(entrya, entryb){
+	return entryb[1] - entrya[1];
 }
